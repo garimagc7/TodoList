70f59462e2550a4d8692db024fde449d3b9bdac9
garimagc7 <garimagc7@gmail.com>
2026-08-07 17:06:09 +0545

    showingTodoList

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let todoList = [{
     name: 'washing dishes' ,
     date: 2026-9-18
-}]
+}];
+showInput();
 function showInput(){
+    let todoListHTML = '';
 
-
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, date } = todoObject;
+        const html = `
+        <div>${name}</div>
+        <div>${date}</div>
+        <button onclick="
+            todoList.splice(${i}, 1);
+            showInput();
+        " class="delete-todo-button">Delete</button> 
+        `;
+        todoListHTML += html;
+    }
+    document.querySelector('.showtodo-list').innerHTML = todoListHTML;
 }
 
 function jsInput(){
@@ -15,5 +32,5 @@ function jsInput(){
 
     todoList.push({name,date});
     todoname.value = '';
-
+    showInput();
 }
